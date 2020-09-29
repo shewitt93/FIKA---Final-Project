e5b43b3448f82b104d3231abce50184bc9c496b1
Simon <simon@Simons-iMac.local>
2020-09-29 12:05:42 +0100

    footer+home page

--- fikafe/src/components/DashBoard.js
+++ fikafe/src/components/DashBoard.js
@@ -8,8 +8,24 @@ class DashBoard extends Component {
   }
 
   render() {
-    console.log(this.props.user);
-    return <div>home</div>;
+    console.log(this.props.user.userData);
+    return (
+      <div className="wrapper">
+        <h3>Welcome {this.props.user.userData.username}</h3>
+        <h6>last login: {this.props.user.userData.last_login}</h6>
+        <h5>What would you like to do today?</h5>
+        <br></br>
+        <div className="game1">
+          <a href="/home/game">TIKTAKTOE</a>
+        </div>
+        <div className="game2">
+          <a href="/home/game">BLACKJACK</a>
+        </div>
+        <div className="game3">
+          <a href="/home/game">CHESS</a>
+        </div>
+      </div>
+    );
   }
 }
 const mSTP = (state) => ({ user: state });
---
+++ fikafe/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { NavLink } from "react-router-dom";
+// import "../styles/NavBar.css";
+// import { logout } from "../reducer/authReducer";
+
+export default function Footer() {
+  return (
+    <div className="navContainer">
+      {" "}
+      <h1 className="logo">hello there</h1>
+    </div>
+  );
+}
